38e1fe862613cda45089f9fcc9a6ea98cf63fd2d
BondarenkoAlla <bondarenkoalla961@gmail.com>
2021-09-23 13:56:26 +0300

    2

--- index.js
+++ index.js
@@ -3,33 +3,30 @@ const {
   getContactById,
   addContact,
   removeContact,
-} = require('./contacts.js');
+} = require("./contacts.js");
 
-
-const argv = require('yargs').argv;
-
+const argv = require("yargs").argv;
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case 'list':
-          listContacts();
-          
+    case "list":
+      listContacts();
       break;
 
-    case 'get':
-     getContactById(id);
+    case "get":
+      getContactById(id);
       break;
 
-    case 'add':
-        addContact(name, email, phone);
+    case "add":
+      addContact(name, email, phone);
       break;
 
-    case 'remove':
-         removeContact(id);
+    case "remove":
+      removeContact(id);
       break;
 
     default:
-      console.warn('\x1B[31m Unknown action type!');
+      console.warn("\x1B[31m Unknown action type!");
   }
 }
 
